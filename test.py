df2ea6eefc4d92600bae49bf59f4c1bddfe0bd4d
SeanLeRoy <seanm@alleninstitute.org>
2023-11-20 14:33:11 -0800

    Remove data files

--- test.py
+++
@@ -1,8 +0,0 @@
-extensions = []
-with open('data.txt', 'r') as reader:
-    for line in reader:
-        extensions.append(line)
-    
-output = ",\n".join([f"\"{extension[:len(extension) - 1]}\"" for extension in sorted(list(set(extensions)))])
-with open("output.txt", "w") as writer:
-    writer.write(output)
