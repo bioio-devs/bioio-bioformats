b282ce60d945bd2420badb6c5997373eef9cbd7e
SeanLeRoy <seanme97@gmail.com>
2025-07-21 11:19:08 -0700

    Test excluding failure prone files

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -14,6 +14,7 @@ assert test_resources_dir.exists(), f"Test resources directory {test_resources_d
 test_files = [
     test_file
     for test_file in test_resources_dir.iterdir()
+    if test_file.name not in {"example.txt", "ND2_aryeh_but3_cont200-1.nd2"}
 ]
 print(f"Test files: {[file.name for file in test_files]}")
 bioio_base.benchmark.benchmark(bioio_bioformats.reader.Reader, test_files)
